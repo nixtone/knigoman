280dbbb8dd887cfefec28ebdb431e20d34f54a6f
Nixtone <nixtone@nxt.ru>
2024-02-21 21:25:15 +0300

    База, сиды

---
+++ README.md
@@ -0,0 +1,33 @@
+# Книгоман  
+
+__Описание:__ Тестовое задание на должность разработчика в компанию ТТК  
+
+
+## Техническая записка  
+
+- PHP 8.1  
+- MySQL 8.0  
+- HTML  
+- CSS  
+- Верстка (валидная, адаптивная)  
+
+### Вспомогательные сервисы  
+
+Оптимизация графики: [tinypng.com](https://tinypng.com/)  
+
+### Источники  
+
+- [Лого](https://www.svgrepo.com/svg/492785/book-and-person-summer)  
+
+
+## Деплой  
+
+1. Сгенерировать .env  
+2. php artisan key:generate  
+3. php artisan migrate --seed  
+
+## БД  
+
+book_category  
+book  
+author  
---
+++ database/migrations/2024_02_21_163240_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->smallInteger('publish_year');
+            $table->text('descr');
+            // обложка считывается из "storage" по id кинги
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
